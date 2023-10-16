d1ba5dc0effef76106facfad5a75ac4e9d3fe487
diakhite <diakhite270@outlook.fr>
2023-10-16 13:53:45 -0700

    Script 0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -11,4 +11,12 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 	return (0);
-}
+ if (n > 0) {
+printf
+     } else if (n < 0) {
+        printf("%d est négatif\n", n);
+    } else {
+        printf("%d est égal à zéro\n", n);
+    }
+return 0;
+}}
