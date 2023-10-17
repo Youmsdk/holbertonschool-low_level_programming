b45be1c7fde60d38a68126e7805fd7dd26100513
diakhite <diakhite270@outlook.fr>
2023-10-17 13:40:49 -0700

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (d > 5)
-		{				
+
 		printf("Last digit of %d is %d and is greater than 5\n", n, d);
 	}
 	else if (d < 6 && d != 0)
@@ -27,3 +27,4 @@ int main(void)
 		printf("Last digit of %d is %d and is 0\n", n, d);
 	}
 	return (0);
+}
