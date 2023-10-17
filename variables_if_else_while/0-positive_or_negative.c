49287a5792ac6eb781ef7e022809f2c4b94fbb73
diakhite <diakhite270@outlook.fr>
2023-10-17 05:21:21 -0700

    0-profile_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,5 +1,5 @@
 #include <stdlib.h>
-#include <time.h> 
+#include <time.h>
 #include <stdio.h>
 
 /**
@@ -8,27 +8,23 @@
  * Return: 0 (if success)
  */
 int main(void)
+
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	{      	 
-	         
+	{
 		printf("%d is positive\n", n);
-	}	
-        else if (n == 0) 
-        {
-	
-		printf("%d is zero\n", n); 
-        }
-	
-	else            
-	{ 
-		 
-		 printf("%d negative\n", n);
-        }
-        
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d negative\n", n);
+	}
 	return (0);
 }
