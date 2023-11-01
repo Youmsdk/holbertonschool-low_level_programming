790220b475c46165f9b6bb0ad504facb4eb1c0cb
diakhite <diakhite270@outlook.fr>
2023-11-01 06:08:25 -0700

    1-print_rev_recursion.c

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -5,6 +5,7 @@
  * _print_rev_recursion - prints a string in reverse
  * @s:string
  * Return: on sucess 1.
+ * on error, -1 is returned, errno is set appropriately
  */
 void _print_rev_recursion(char *s)
 {
