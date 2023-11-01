665a2d78a667e40dfbd34aaad0956dcf5422ea9c
diakhite <diakhite270@outlook.fr>
2023-11-01 13:17:48 -0700

    3-factorial.c

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -2,17 +2,16 @@
 #include <string.h>
 #include <stdio.h>
 /*
- * _strlen_recursion - Returns the length of a string.
- * @s: string
+ * _strlen_recursion - Returns the length of a string
+ * @s: string to calculate the length of
  *
- * Return: lenght of a string
- * On error, -1 is returned, and errno is set appropriately
+ * Return: length of the string
 */
 int _strlen_recursion(char *s)
 {
 	if (*s)
 	{
-		return (_strlen_recursion(s + 1));
+		return (1 + _strlen_recursion(s + 1));
 
 	}
 	return (0);
